903431aa4726a84385fb452e8c45884b66e04b31
Nathaniel Louis Tisuela <natetisuela@gmail.com>
2021-02-06 17:16:01 -0800

    totally didn't commit my api key

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+
+// We use fetch for this example. A popular alternative is axios.
 const fetch = require('node-fetch');
 
 // HTTPS does not work right now! 
 const url = "http://api.peterportal.org/graphql"; 
 
+// The query is written as a string -- We'll JSONify it laterrr
 const query = `
     query {
         course(id: "DANCE3"){
@@ -17,12 +20,21 @@ const query = `
     }
 `;
 
+
+// The options to be used as arguments when we call fetch
 const options = {
+
+    // This contains our query. We send it in the body of the request.
     body: JSON.stringify({query}),
+
+    // Because our request contains information in the body, we must send it as a POST request
+    // All clients consuming a GraphQL API must use POST!!
     method: "POST",
+
+    // Not necessary for every Web API, but it's good practice (and required for PeterPortal API)
     headers : {
         "Content-Type": "application/json",
-        "x-api-key": "graphql_example-5649cc123aff6b345ee2e83827006679d88490812d6c525761dae2274ad680b4"
+        "x-api-key": "get your API key here: http://api.peterportal.org/docs/Getting-Started/Register-Here/"
     }
     
     
